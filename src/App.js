06aaa6aab9ee94bbfc6ef77b886a5b5d861b5202
blackman2xxx@gmail.com <blackman2xxx>
2021-11-27 08:54:29 +0700

    add menu

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import "styles/globalStyles.css";
 import React from "react";
 import { css } from "styled-components/macro"; //eslint-disable-line
 
+import tw from "twin.macro";
+// import { css } from "styled-components/macro"; //eslint-disable-line
+// import AnimationRevealPage from "helpers/AnimationRevealPage.js";
+// import Hero from "components/hero/TwoColumnWithVideo.js";
+// import Features from "components/features/ThreeColSimple.js";
+// import MainFeature from "components/features/TwoColWithButton.js";
+// import MainFeature2 from "components/features/TwoColSingleFeatureWithStats2.js";
+import TabGrid from "components/cards/TabCardGrid.js";
+// import Testimonial from "components/testimonials/ThreeColumnWithProfileImage.js";
+// import DownloadApp from "components/cta/DownloadApp.js";
+// import Footer from "components/footers/FiveColumnWithInputForm.js";
+
+// import chefIconImageSrc from "images/chef-icon.svg";
+// import celebrationIconImageSrc from "images/celebration-icon.svg";
+// import shopIconImageSrc from "images/shop-icon.svg";
+
 /*
  * This is the entry point component of this project. You can change the below exported default App component to any of
  * the prebuilt landing page components by uncommenting their import and export lines respectively.
@@ -112,7 +128,11 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 export default function App() {
   // If you want to disable the animation just use the disabled `prop` like below on your page's component
   // return <AnimationRevealPage disabled>xxxxxxxxxx</AnimationRevealPage>;
-
+  const Subheading = tw.span`tracking-wider text-sm font-medium`;
+  const HighlightedText = tw.span`bg-primary-500 text-gray-100 px-4 transform -skew-x-12 inline-block`;
+  const HighlightedTextInverse = tw.span`bg-gray-100 text-primary-500 px-4 transform -skew-x-12 inline-block`;
+  const Description = tw.span`inline-block mt-8`;
+  const imageCss = tw`rounded-4xl`;
 
   return (
     <Router>
@@ -134,6 +154,13 @@ export default function App() {
         </Route>
         <Route path="/">
           <MainLandingPage />
+          <TabGrid
+        heading={
+          <>
+            Checkout our <HighlightedText>clothes </HighlightedText>
+          </>
+        }
+      />
         </Route>
       </Switch>
     </Router>
--- src/components/cards/TabCardGrid.js
+++ src/components/cards/TabCardGrid.js
@@ -61,91 +61,9 @@ const DecoratorBlob2 = styled(SvgDecoratorBlob2)`
 export default ({
   heading = "Checkout the Menu",
   tabs = {
-    Starters: [
-      {
-        imageSrc:
-          "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
-        title: "Veg Mixer",
-        content: "Tomato Salad & Carrot",
-        price: "$5.99",
-        rating: "5.0",
-        reviews: "87",
-        url: "#"
-      },
-      {
-        imageSrc:
-          "https://images.unsplash.com/photo-1432139555190-58524dae6a55?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
-        title: "Macaroni",
-        content: "Cheese Pizza",
-        price: "$2.99",
-        rating: "4.8",
-        reviews: "32",
-        url: "#"
-      },
-      {
-        imageSrc:
-          "https://images.unsplash.com/photo-1476224203421-9ac39bcb3327??ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
-        title: "Nelli",
-        content: "Hamburger & Fries",
-        price: "$7.99",
-        rating: "4.9",
-        reviews: "89",
-        url: "#"
-      },
-      {
-        imageSrc:
-          "https://images.unsplash.com/photo-1455619452474-d2be8b1e70cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
-        title: "Jalapeno Poppers",
-        content: "Crispy Soyabeans",
-        price: "$8.99",
-        rating: "4.6",
-        reviews: "12",
-        url: "#"
-      },
-      {
-        imageSrc:
-          "https://images.unsplash.com/photo-1473093226795-af9932fe5856?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
-        title: "Cajun Chicken",
-        content: "Roasted Chicken & Egg",
-        price: "$7.99",
-        rating: "4.2",
-        reviews: "19",
-        url: "#"
-      },
-      {
-        imageSrc:
-          "https://images.unsplash.com/photo-1550461716-dbf266b2a8a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
-        title: "Chillie Cake",
-        content: "Deepfried Chicken",
-        price: "$2.99",
-        rating: "5.0",
-        reviews: "61",
-        url: "#"
-      },
-      {
-        imageSrc:
-          "https://images.unsplash.com/photo-1534422298391-e4f8c172dddb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
-        title: "Guacamole Mex",
-        content: "Mexican Chilli",
-        price: "$3.99",
-        rating: "4.2",
-        reviews: "95",
-        url: "#"
-      },
-      {
-        imageSrc:
-          "https://images.unsplash.com/photo-1565310022184-f23a884f29da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
-        title: "Carnet Nachos",
-        content: "Chilli Crispy Nachos",
-        price: "$3.99",
-        rating: "3.9",
-        reviews: "26",
-        url: "#"
-      }
-    ],
-    Main: getRandomCards(),
-    Soup: getRandomCards(),
-    Desserts: getRandomCards()
+    DailyClothes: getDailyClothes(),
+    SportClothes: getSportClothes(),
+
   }
 }) => {
   /*
@@ -234,86 +152,94 @@ export default ({
 };
 
 /* This function is only there for demo purposes. It populates placeholder cards */
-const getRandomCards = () => {
+const getDailyClothes = () => {
   const cards = [
     {
       imageSrc:
-        "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
-      title: "Chicken Chilled",
-      content: "Chicken Main Course",
-      price: "$5.99",
+        "https://mcdn2.coolmate.me/uploads/November2021/DSC07107_copy.jpg",
+      title: "Áo Hoodie nam có mũ trùm",
+      price: "389.000đ",
       rating: "5.0",
-      reviews: "87",
+      reviews: "90",
       url: "#"
     },
     {
       imageSrc:
-        "https://images.unsplash.com/photo-1582254465498-6bc70419b607?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
-      title: "Samsa Beef",
-      content: "Fried Mexican Beef",
-      price: "$3.99",
-      rating: "4.5",
-      reviews: "34",
-      url: "#"
-    },
-    {
-      imageSrc:
-        "https://images.unsplash.com/photo-1565310022184-f23a884f29da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
-      title: "Carnet Nachos",
-      content: "Chilli Crispy Nachos",
-      price: "$3.99",
-      rating: "3.9",
-      reviews: "26",
-      url: "#"
-    },
-    {
-      imageSrc:
-        "https://images.unsplash.com/photo-1534422298391-e4f8c172dddb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
-      title: "Guacamole Mex",
-      content: "Mexican Chilli",
-      price: "$3.99",
-      rating: "4.2",
-      reviews: "95",
-      url: "#"
-    },
-    {
-      imageSrc:
-        "https://images.unsplash.com/photo-1550461716-dbf266b2a8a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
-      title: "Chillie Cake",
-      content: "Deepfried Chicken",
-      price: "$2.99",
+        "https://mcdn2.coolmate.me/uploads/November2021/denn3-(2).jpg",
+      title: "Áo thun Cotton Compact",
+      price: "259.000đ",
       rating: "5.0",
-      reviews: "61",
+      reviews: "90",
       url: "#"
     },
     {
       imageSrc:
-        "https://images.unsplash.com/photo-1476224203421-9ac39bcb3327??ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
-      title: "Nelli",
-      content: "Hamburger & Fries",
-      price: "$7.99",
-      rating: "4.9",
-      reviews: "89",
+        "https://mcdn2.coolmate.me/uploads/November2021/1-0_copyu_672x990.jpg",
+      title: "Áo Polo Cotton Compact",
+      price: "202.000đ",
+      rating: "5.0",
+      reviews: "90",
       url: "#"
     },
     {
       imageSrc:
-        "https://images.unsplash.com/photo-1455619452474-d2be8b1e70cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
-      title: "Jalapeno Poppers",
-      content: "Crispy Soyabeans",
-      price: "$8.99",
-      rating: "4.6",
-      reviews: "12",
+        "https://mcdn2.coolmate.me/uploads/November2021/1-0_copy1.jpg",
+      title: "Quần Jeans Basic Slim",
+      price: "499.000đ",
+      rating: "5.0",
+      reviews: "90",
+      url: "#"
+    }
+  ];
+
+  // Shuffle array
+  return cards.sort(() => Math.random() - 0.5);
+};
+const getSportClothes = () => {
+  const cards = [
+    {
+      imageSrc:
+        "https://mcdn2.coolmate.me/uploads/November2021/IMG_68181.jpg",
+      title: "Quần Jogger Sweatpants ",
+      price: "269.000đ",
+      rating: "5.0",
+      reviews: "90",
       url: "#"
     },
     {
       imageSrc:
-        "https://images.unsplash.com/photo-1473093226795-af9932fe5856?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80",
-      title: "Cajun Chicken",
-      content: "Roasted Chicken & Egg",
-      price: "$7.99",
-      rating: "4.2",
-      reviews: "19",
+        "https://mcdn2.coolmate.me/uploads/November2021/2_26_copy.jpg",
+      title: "Áo Polo ProMax-S1 Logo",
+      price: "239.000đ",
+      rating: "5.0",
+      reviews: "90",
+      url: "#"
+    },
+    {
+      imageSrc:
+        "https://mcdn2.coolmate.me/uploads/November2021/2L6A6164(1).jpg",
+      title: "Quần Jogger co giãn ",
+      price: "229.000đ",
+      rating: "5.0",
+      reviews: "90",
+      url: "#"
+    },
+    {
+      imageSrc:
+        "https://mcdn2.coolmate.me/uploads/November2021/1426x2100_(4).jpg",
+      title: "Áo Tank Top Dri-Breathe ",
+      price: "189.000đ",
+      rating: "5.0",
+      reviews: "90",
+      url: "#"
+    },
+    {
+      imageSrc:
+        "https://mcdn2.coolmate.me/uploads/November2021/mus2_copy.jpg",
+      title: "Quần Max Ultra Short",
+      price: "189.000đ",
+      rating: "5.0",
+      reviews: "90",
       url: "#"
     }
   ];
